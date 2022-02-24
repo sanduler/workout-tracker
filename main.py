25b3bba78dcbaa9d76503c03bf48a77655b16e7e
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-02-24 10:08:17 -0800

    added the apy key and id in the local environment.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Ruben Sanduleac
-
-
+import os
+import requests
 
 # TODO: Create a "My Workouts" Google sheet that will be used to store all the information about the workouts.
 # TODO: Use the Nutrionix API for the natural language engine
 # TODO: GET the API key and access the request.
+
+APP_ID = os.environ["APP_ID"]
+API_KEY = os.environ["API_KEY"]
